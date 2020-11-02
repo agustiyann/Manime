6672337526dd3775d285362a6c8801c65b6f629a
agustiyann <agustiyaninfo@gmail.com>
2020-11-02 08:54:18 +0700

    :heavy_plus_sign: Add okHttpClient interceptor

--- app/src/main/java/com/masscode/manime/data/source/remote/network/ApiConfig.kt
+++ app/src/main/java/com/masscode/manime/data/source/remote/network/ApiConfig.kt
@@ -2,8 +2,11 @@ package com.masscode.manime.data.source.remote.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiConfig {
 
@@ -11,9 +14,15 @@ object ApiConfig {
     private val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
     init {
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.jikan.moe/v3/")
             .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
             .build()
         api = retrofit.create(ApiServices::class.java)
     }
