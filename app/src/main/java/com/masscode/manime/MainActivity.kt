502de069603945113611f56017deb05d2c9d327f
agustiyann <agustiyaninfo@gmail.com>
2020-11-03 22:52:20 +0700

    :sparkles: Add drawer menu

--- app/src/main/java/com/masscode/manime/MainActivity.kt
+++ app/src/main/java/com/masscode/manime/MainActivity.kt
@@ -4,19 +4,30 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.masscode.manime.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navCtrl: NavController
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        setSupportActionBar(binding.mainToolbar)
-        val sword = String(Character.toChars(0x2694))
-        binding.toolbarTitle.text = "$sword ${binding.mainToolbar.title}"
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+        setSupportActionBar(main_toolbar)
+        main_toolbar.setTitleTextAppearance(this, R.style.ElectroHarmonixFont)
+        navCtrl = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navCtrl, drawer_layout)
+        NavigationUI.setupWithNavController(nav_view, navCtrl)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navCtrl, drawer_layout)
     }
 }
